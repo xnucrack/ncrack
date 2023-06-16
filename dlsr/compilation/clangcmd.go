6a41cae9bc0d21b08f9ddbccc04af9ea5ae4a013
NSEcho <xdaemonx@protonmail.ch>
2023-06-16 22:21:44 +0200

    feat: create .h and .m files in temp directory for specific source

--- dlsr/compilation/clangcmd.go
+++ dlsr/compilation/clangcmd.go
@@ -15,6 +15,12 @@ import (
 var fileTemplates embed.FS
 
 func Compile(c parsing.Codebase) error {
+	dir, err := os.MkdirTemp("/tmp/", "dlsr")
+	if err != nil {
+		return fmt.Errorf("error creating temp directory: %v", err)
+	}
+	// defer os.RemoveAll(dir)
+
 	t, err := template.New("").Funcs(template.FuncMap{
 		"baseFileName": func(path string) string {
 			return strings.TrimSuffix(path, filepath.Ext(path))
@@ -24,9 +30,31 @@ func Compile(c parsing.Codebase) error {
 		return err
 	}
 
-	t.ExecuteTemplate(os.Stdout, "header", c.Sources[0])
-	fmt.Println("=====================================")
-	t.ExecuteTemplate(os.Stdout, "main", c.Sources[0])
+	for _, source := range c.Sources {
+		source.Path = "dlsr_" + source.Path
+		err := func() error {
+			base := strings.TrimSuffix(source.Path, filepath.Ext(source.Path))
+
+			headerFile, err := os.Create(filepath.Join(dir, base+".h"))
+			if err != nil {
+				return err
+			}
+			defer headerFile.Close()
+
+			t.ExecuteTemplate(headerFile, "header", source)
+
+			mainFile, err := os.Create(filepath.Join(dir, base+".m"))
+			if err != nil {
+				return err
+			}
+			defer headerFile.Close()
+			t.ExecuteTemplate(mainFile, "main", source)
+			return nil
+		}()
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
